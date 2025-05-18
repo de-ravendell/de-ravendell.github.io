a1cf026363e48dd4c48500d9dea0e5cd363d9d62
de-ravendell <ravendell22@outlook.com>
2025-05-18 20:08:32 +0800

    Update check.js

--- check.js
+++ check.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async function() {
         if (sessionStorage.getItem('authenticated') !== 'true') {
-                 window.open('/');
+                 window.open('https://de-ravendell.github.io');
         }
 });
